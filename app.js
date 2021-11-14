1bc45b6ae2e49e0ae9c3353ae0c2ee9a03c9a1d7
AprilS21 <asheeran@tcd.ie>
2021-11-14 19:50:05 +0000

    converted incoming data to json and displayd login

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+async function main(){
 console.log("testing js1234");
 let url = `https://api.github.com/users/AprilS21`;
 
-getRequest(url, "AprilS21")
+var info = await getRequest(url, "AprilS21");
+console.log(info);
+let name = document.getElementById('test');
+    name.innerHTML = `<b>Name2: </b>${info.login}`;
+}
+
 
 async function getRequest(url, token) {
 
-let object = await fetch(url,{
+/* let result = await fetch(url,{
     "method": "GET",
     "user-agent": token
-});
+}); */
+var data;
 
-console.log(object.json());
+let response  = await fetch(url,{
+    "method": "GET",
+    "user-agent": token
+})
+.then(response=> response.json());
+
+console.log(response);
+
+//console.log(object.json());
+
+//var info = object.json();
+data = response
+return data
+
 }
