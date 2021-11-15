38e21121802811840703ee0e3e5dedc33622dd3c
AprilS21 <asheeran@tcd.ie>
2021-11-15 21:45:49 +0000

    linechart commits per given repo, profile picture

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ let url = `https://api.github.com/users/AprilS21`;
 
 var info = await getRequest(url, "AprilS21");
 console.log(info);
-let name = document.getElementById('test');
-name.innerHTML = `<b>Name2: </b>${info.login}`;
+let name = document.getElementById('login');
+name.innerHTML = `<b>Name: </b>${info.login}`;
+
+let image = document.getElementById('image');
+image.src=info.avatar_url;
 
 url = `https://api.github.com/users/AprilS21/repos`
 var repo = await getRequest(url, "AprilS21");
-console.log(repo);
+//console.log(repo);
 
-name = document.getElementById('test2');
+/* name = document.getElementById('test2');
  for(i in repo){
     name.innerHTML='Repo id111: ' +repo[i].id; 
     console.log(repo[i].id);
-} 
+}  */
 /* url = `https://api.github.com/repos/AprilS21/lowestCommonAncestor/commits`
 var commits = await getRequest(url, "AprilS21");
 console.log(commits); */
 
-drawLineChart(repo, "AprilS21");
+await drawBarChart(repo, "AprilS21");
+await drawLineChart(repo,"AprilS21","lowestCommonAncestor");
 }
 
 
@@ -39,7 +43,7 @@ data = response
 return data
 }
 
-async function drawLineChart(repo, token){
+async function drawBarChart(repo, token){
 /* var xValues = [50,60,70,80,90,100,110,120,130,140,150];
 var yValues = [7,8,8,9,9,9,10,11,14,14,15]; */
 
@@ -72,3 +76,45 @@ new Chart("myChart", {
     }
   });
 }
+
+async function drawLineChart(repo, token, request){
+    var url ="https://api.github.com/repos/"+token;
+    var xValues = [];
+    var yValues = [];
+
+    commit = await getRequest(url + "/"+request+"/stats/participation");
+        for(j in commit.all){
+            yValues.push(commit.all[j]);
+            xValues.push(j);
+        }
+      //  console.log(commit.count);
+    console.log(yValues+"array!!222222")
+
+    new Chart("myChart2", {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,0.1)",
+          data: yValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title:{
+          display:true,
+          text: "Commits per week in " + request
+        },
+        scales: {
+          yAxes: [{ticks: {min: 0, max: Math.max(...yValues)}}],
+        }
+      }
+    });
+
+}
+
+
+
